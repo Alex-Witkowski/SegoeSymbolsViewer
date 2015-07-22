41b395a27033ea4ecb375ef9927227fe006313ff
Alex-Witkowski <alexanderwitkowski@gmail.com>
2015-07-22 10:29:49 +0200

    Initial commit

---
+++ SegoeSymbolsViewer/MainPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SegoeSymbolsViewer
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            var list = new List<SymbolPoco>();
+            for (int i = 0xE001; i <= (0xEC7A); i++)
+            {
+                var poco = new SymbolPoco(i);
+                list.Add(poco);
+            }
+
+            listView.ItemsSource = list;
+        }
+
+        private void AppBarButtonCopyCode_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(listView.SelectedItem is SymbolPoco))
+            {
+                return;
+            }
+
+            var poco = (SymbolPoco) listView.SelectedItem;
+            CopyText(poco.HexText);
+        }
+
+        private void AppBarButtonCopyXaml_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(listView.SelectedItem is SymbolPoco))
+            {
+                return;
+            }
+
+            var poco = (SymbolPoco)listView.SelectedItem;
+            CopyText(poco.XamlText);
+        }
+
+        private static void CopyText(string text)
+        {
+            var dataPackage = new DataPackage();
+            dataPackage.SetText(text);
+
+            try
+            {
+                Clipboard.SetContent(dataPackage);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ SegoeSymbolsViewer/SymbolPoco.cs
@@ -0,0 +1,16 @@
+﻿namespace SegoeSymbolsViewer
+{
+    public struct SymbolPoco
+    {
+        public SymbolPoco(int i)
+        {
+            this.Symbol = char.ConvertFromUtf32(i);
+            this.XamlText = "&#x" + i.ToString("X") + ";";
+            this.HexText = "\\u" + i.ToString("X");
+        }
+
+        public string Symbol { get; set; } 
+        public string XamlText { get; set; } 
+        public string HexText { get; set; } 
+    }
+}
